53382ee3984cf4d10728ee0ecfb6c33285a11caa
Edwards <1548048366@pp.com>
2020-09-25 09:40:42 +0800

    这是二次日志

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {sum} from './js/aaa'
+import {name, age, height} from './js/info'
+import css from './css/main.css';
+import style from './css/main.css'
+console.log(name)
+console.log(sum(12, 34))
+console.log(age);
+console.log(height)
---
+++ src/js/aaa.js
@@ -0,0 +1,4 @@
+export var name ='魔法少女小圆';
+export function sum(num1, num2) {
+  return num1 + num2;
+}
